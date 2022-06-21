cf47cd2fd3de0522e2c2036cc26d59735c7e9a46
Victoria <adedayovicky123@gmail.com>
2022-06-21 17:22:23 +0100

    remov btn when clicked

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function printInfo() {
+  if (btn.style.display === "none") {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
   document.querySelector(".name").textContent = "Name: Victoria Adedayo";
   document.querySelector(".height").textContent = "Height: 6.8ft";
-  document.querySelector(".country").textContent = "Country: Nigerian";
+  document.querySelector(".country").textContent = "Country: Nigeria";
 }
 
-document.querySelector(".btn").addEventListener("click", printInfo);
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", printInfo);
